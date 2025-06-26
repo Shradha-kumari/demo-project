564dce1c4f4622658d941442a92a0a3c26cd1db8
Shradha Kumari <63179408+Shradha-kumari@users.noreply.github.com>
2025-06-27 00:28:08 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<p>Hello, World from india!</p>"
     #message = os.getenv("CUSTOM_MESSAGE", "Thanks, Ashu!")
     #return f"<p>{message}</p>"
     
